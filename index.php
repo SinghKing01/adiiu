1d6936af3852ad1639a8dd7f7b05b99187f37697
SinghKing99 <trabajoescolarsingh@gmail.com>
2022-01-01 13:48:04 +0100

    link to home page

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       <!-- Navigation-->
       <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
          <div class="container">
-            <a class="navbar-brand" href="#page-top">University Dashboard</a>
+            <a class="navbar-brand" href="#index.php">University Dashboard</a>
             <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button"
                data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
                aria-expanded="false" aria-label="Toggle navigation">
@@ -36,7 +36,7 @@
                   <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
                      href="https://www.uib.cat/">Uib</a></li>
                   <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
-                     href="https://github.com/SinghKing99">Datos</a></li>
+                     href="https://github.com/SinghKing99/adiiu">Datos</a></li>
                </ul>
             </div>
          </div>
